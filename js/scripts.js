4e27414e5a7cf4281ebd00e1f16157e4b9a903ed
agatakolohe <agatakolohe@gmail.com>
2020-11-06 11:04:14 -0800

    create var for all inputs, new pizza and topping objects

--- js/scripts.js
+++ js/scripts.js
@@ -9,11 +9,11 @@ function Pizza(size, name) {
 Pizza.prototype.addToppings = function (topping) {
   this.toppings.push(topping);
 };
-function Topping(cheese, meat, veggies) {
-  this.cheese = cheese;
-  this.meat = meat;
-  this.veggies = veggies;
-}
+// function Topping(cheese, meat, veggies) {
+//   this.cheese = cheese;
+//   this.meat = meat;
+//   this.veggies = veggies;
+// }
 Pizza.prototype.toppingPrice = function () {
   if (this.toppings.length >= 5) {
     return (this.total += 15);
@@ -41,3 +41,28 @@ Pizza.prototype.addTotal = function () {
 Pizza.prototype.showTotal = function () {
   return this.name + ", your total is: $" + this.grandTotal;
 };
+//Business Logic for Topping()
+function Topping(cheese, meat, veggies) {
+  this.cheese = cheese;
+  this.meat = meat;
+  this.veggies = veggies;
+}
+// User Interface Logic
+//let pizzaOrder = new Pizza(); /// idk if this needs to be here
+
+$(document).ready(function () {
+  $("form#pizza-order").submit(function (event) {
+    event.PreventDefault();
+    const inputtedName = $("input#name").val();
+    const inputtedPizzaSize = $("input:radio[name=pizza-size]:checked").val();
+    const inputtedCheese = $("input:checkbox[name=cheese]:checked").val();
+    const inputtedProtein = $("input:checkbox[name=protein]:checked").val();
+    const inputtedVeggies = $("input:checkbox[name=veggies]:checked").val();
+    let newPizzaOrder = new Pizza(inputtedName, inputtedPizzaSize);
+    let newToppings = new Topping(
+      inputtedCheese,
+      inputtedProtein,
+      inputtedVeggies
+    );
+  });
+});
